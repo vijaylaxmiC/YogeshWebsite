17af6e307397a72cf1e8048ed9765fd0e726c3a6
Vidhyadhar <vidhyadhar.lonarmath@amadeus.com>
2019-02-23 13:43:48 +0530

    contact us implemntation

--- js/custom.js
+++ js/custom.js
@@ -1,12 +1,12 @@
 var awardsText =  {
-	'awards/a1.jpg':"<h4>A meaningful salutations through dance : Prajavani news paper - Nanjundrao</h4>",
-	'awards/a2.jpg':"",
-	'awards/a3.jpg':"<h4>A show that enhances your love for Indian culture :</h4>",
-	'awards/a4.jpg':"<h4>'Krishnapremi' - a dance fest which opened the doors of experimentations :</h4>",
-	'awards/a5.jpg':"",
-	'awards/a6.jpg':"",
-	'awards/a7.jpg':"<h4>Set the stage on fire through his dynamic bharatanatyam for the theme 'Agni' - Subbu Kriahna</h4>",	
-	'awards/a8.jpg':"",
+	'awards/a1.jpg':"A meaningful salutations through dance : Prajavani news paper - Nanjundrao",
+	'awards/a2.jpg':"'Nauka Charitam' directed by Guru Padmini Ravi",
+	'awards/a3.jpg':"A show that enhances your love for Indian culture",
+	'awards/a4.jpg':"'Krishnapremi' - a dance fest which opened the doors of experimentations",
+	'awards/a5.jpg':"A strong proclamation of 'Guru Shisya parampara'",
+	'awards/a6.jpg':"Bharatanatyam exponent Pratibha Prahlad leaves audience and thrall",
+	'awards/a7.jpg':"Set the stage on fire through his dynamic bharatanatyam for the theme 'Agni' - Subbu Kriahna",	
+	'awards/a8.jpg':"Performed  at Koviayil thiruvaiyaru festival",
 }
 
 function createGallery(){
@@ -44,7 +44,9 @@ function createAwards(){
 	for (var i=1; i<=8; i++) {
 		var photoDiv = document.createElement('div')
 		photoDiv.className = "gallery_image_div"
-		var imgHTML = '<img class= "gallery-img" src="awards/a'+i+'.jpg" onclick="AwardsImageClicked(this)">';
+		var imgHeading = 'awards/a'+i+'.jpg'
+		var imgHTML = '<p class ="award_heading">'+awardsText[imgHeading]+'</p>'
+		imgHTML += '<img class= "gallery-img" src="awards/a'+i+'.jpg" onclick="AwardsImageClicked(this)">';
 		photoDiv.innerHTML = imgHTML;
 		parentElement.appendChild(photoDiv)
 	}
@@ -58,6 +60,31 @@ function AwardsImageClicked(el){
 	var imgHTML = '<img src = "'+imgSrc+'">'
 	popoveModalContent.innerHTML = imgHTML
 	$('#popover_modal').fadeIn()	
+}
+
+function sendEmail() {
+
+	var content = document.getElementById('contactus_message').value+'<br>My Contact number is: '+document.getElementById('contactus_phone').value+'<br>My email is:'+ document.getElementById("contactus_email").value;
+
+	  var template_params = {
+	     "reply_to": document.getElementById('contactus_email'),
+	     "from_name": document.getElementById('contactus_name').value,
+	     "to_name": "Yogesh Kumar",
+	     "message_html": content
+	  }
+
+	  var service_id = "default_service";
+	  var template_id = "template_C3qAmeiU";
+	  emailjs.send(service_id, template_id, template_params);
+
+
+	  document.getElementById('contactus_message').value = ""
+	  document.getElementById('contactus_phone').value = ""
+	  document.getElementById('contactus_email').value = ""
+	  document.getElementById('contactus_name').value = ""
+
+	  alert("Thank you for reaching out to us!")
+
 }
 
 
